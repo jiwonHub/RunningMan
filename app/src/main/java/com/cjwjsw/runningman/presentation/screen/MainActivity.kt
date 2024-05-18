36706a5b8f372f1b13c37d7c520abd75b3f34d26
jiwonHub <jiwon30931@gmail.com>
2024-05-18 22:49:47 +0900

    [FIX] package rename

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/MainActivity.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.runningman.presentation.screen
+package com.cjwjsw.runningman.presentation.screen
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.runningman.R
+import com.cjwjsw.runningman.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
