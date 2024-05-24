92d421d2808a90e70d2cbf64d2c761fe58b0b40e
jiwon Choi <jiwon31273@naver.com>
2024-05-24 11:10:50 +0900

    [ADD] LoginViewModel.kt googleLogin add

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/login/LoginViewModel.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/login/LoginViewModel.kt
@@ -1,22 +1,43 @@
 package com.cjwjsw.runningman.presentation.screen.login
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
-import android.content.Context
+import android.content.ContextWrapper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.cjwjsw.runningman.R
+import com.cjwjsw.runningman.data.preference.AppPreferenceManager
+import com.cjwjsw.runningman.domain.factory.LoginViewModelFactory
+import com.cjwjsw.runningman.domain.model.UserModel
+import com.google.firebase.auth.FirebaseUser
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
+@HiltViewModel
+class LoginViewModel @AssistedInject constructor(
+    @Assisted private val contextWrapper: ContextWrapper,
+    private val appPreferenceManager: AppPreferenceManager
+) : ViewModel() {
 
-//https://jgeun97.tistory.com/350
+    val loginStateLiveData = MutableLiveData<LoginState>(LoginState.Uninitialized)
 
-class LoginViewModel(context: Context): AppCompatActivity() {
-    private var context = context
-
-    fun kakaoLogin(){
+    fun kakaoLogin() {
 
         //공용 콜백 선언
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
@@ -25,23 +46,23 @@ class LoginViewModel(context: Context): AppCompatActivity() {
             } else if (token != null) {
                 Log.i(ContentValues.TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
                 UserApiClient.instance.me { user, error ->
-                    if(error != null){
+                    if (error != null) {
                         Log.e(ContentValues.TAG, "사용자 정보 요청 실패", error)
-                    }
-                    else if (user != null) {
+                    } else if (user != null) {
                         //TODO 파베에 사용자 정보 저장
                         Log.i(
                             ContentValues.TAG, "사용자 정보 요청 성공" +
                                     "\n회원번호: ${user.id}" +
                                     "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
-                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}")
+                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+                        )
                     }
                 }
             }
         }
 
-        if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
-            UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+        if (UserApiClient.instance.isKakaoTalkLoginAvailable(contextWrapper)) {
+            UserApiClient.instance.loginWithKakaoTalk(contextWrapper) { token, error ->
                 if (error != null) {
                     Log.e(ContentValues.TAG, "카카오톡으로 로그인 실패", error)
 
@@ -52,16 +73,57 @@ class LoginViewModel(context: Context): AppCompatActivity() {
                     }
 
                     // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
-                    UserApiClient.instance.loginWithKakaoAccount(this, callback = callback )
+                    UserApiClient.instance.loginWithKakaoAccount(contextWrapper, callback = callback)
                 } else if (token != null) {
                     Log.i(ContentValues.TAG, "카카오톡으로 로그인 성공 ${token.accessToken}")
                 }
             }
         } else {
-            UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+            UserApiClient.instance.loginWithKakaoAccount(contextWrapper, callback = callback)
+        }
+    }
+
+    fun saveToken(idToken: String) = viewModelScope.launch {
+        withContext(Dispatchers.IO) {
+            appPreferenceManager.putIdToken(idToken)
+        }
+        fetchData()
+    }
+
+    fun fetchData(): Job = viewModelScope.launch {
+        loginStateLiveData.value = LoginState.Loading
+        appPreferenceManager.getIdToken()?.let {
+            loginStateLiveData.value = LoginState.Login(it)
+        } ?: kotlin.run {
+            loginStateLiveData.value = LoginState.Success.NotRegistered
+        }
+    }
+
+    fun setUserInfo(firebaseUser: FirebaseUser?) = viewModelScope.launch {
+        firebaseUser?.let { user ->
+            appPreferenceManager.saveUserInfo(
+                UserModel(
+                    userId = user.uid,
+                    profileImageUri = user.photoUrl,
+                    userName = user.displayName ?: "익명"
+                )
+            )
+        } ?: kotlin.run {
+            loginStateLiveData.value = LoginState.Success.NotRegistered
         }
     }
 
     //TODO 파베 저장 코드 생성
-    fun setDataToFB(){}
+    fun setDataToFB() {}
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: LoginViewModelFactory,
+            contextWrapper: ContextWrapper
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                return assistedFactory.create(contextWrapper) as T
+            }
+        }
+    }
 }
