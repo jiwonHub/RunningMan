2b91de993aa2c84d12253ecd1974597af0632c10
jiwon Choi <jiwon31273@naver.com>
2024-05-24 11:10:21 +0900

    [ADD] LoginScreen.kt googleLogin add

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/login/LoginScreen.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/login/LoginScreen.kt
@@ -1,16 +1,26 @@
 package com.cjwjsw.runningman.presentation.screen.login
 
+import android.app.Activity
 import android.content.ContentValues
 import android.content.ContentValues.TAG
+import android.content.ContextWrapper
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.cjwjsw.runningman.R
 import com.cjwjsw.runningman.databinding.ActivityLoginBinding
+import com.cjwjsw.runningman.domain.factory.LoginViewModelFactory
 import com.cjwjsw.runningman.presentation.screen.MainActivity
 import com.cjwjsw.runningman.presentation.screen.SplashScreen
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.KakaoSdk
 import com.kakao.sdk.common.model.ClientError
@@ -18,32 +28,125 @@ import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginScreen : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var viewModel: LoginViewModel
+
+    private val firebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    @Inject
+    private lateinit var loginViewModelFactory: LoginViewModelFactory
+
+    private val viewModel by viewModels<LoginViewModel> {
+        LoginViewModel.provideFactory(loginViewModelFactory, ContextWrapper(this))
+    }
+
+    private val gso: GoogleSignInOptions by lazy {
+        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestEmail()
+            .build()
+    }
+
+    private val gsc by lazy { GoogleSignIn.getClient(this, gso) }
+
+    private val loginLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                task.getResult(ApiException::class.java)?.let { account ->
+                    viewModel.saveToken(account.idToken ?: throw Exception())
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
-        KakaoSdk.init(this,"99180739a7bcf290c7df2a47e48e4767")
-
+        KakaoSdk.init(this, "99180739a7bcf290c7df2a47e48e4767")
 
         setContentView(binding.root)
+        initState()
+    }
 
+    private fun initState() {
+        initViews()
+        observeData()
+    }
+
+    private fun initViews() = with(binding) {
+        viewModel.fetchData()
         binding.root.viewTreeObserver.addOnGlobalLayoutListener {
             // 애니메이션 시작
             binding.root.transitionToEnd()
         }
 
-
         binding.kakaoLogin.setOnClickListener {
-               viewModel = LoginViewModel(this)
-               viewModel.kakaoLogin()
-
+            viewModel.kakaoLogin()
         }
 
+        binding.googleLogin.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val signInIntent = gsc.signInIntent
+        loginLauncher.launch(signInIntent)
+    }
+
+    private fun observeData() = viewModel.loginStateLiveData.observe(this) {
+        when(it) {
+            is LoginState.Error -> handleErrorState(it)
+            is LoginState.Loading -> handleLoadingState()
+            is LoginState.Login -> handleLoginState(it)
+            is LoginState.Success -> handleSuccessState(it)
+            else -> Unit
+        }
+    }
+
+    private fun handleLoadingState() {
+
+    }
+
+    private fun handleErrorState(state: LoginState.Error) {
+
+    }
+
+    private fun handleSuccessState(state: LoginState.Success) {
+        when(state) {
+            is LoginState.Success.Registered -> {
+                handleRegisteredState(state)
+            }
+
+            is LoginState.Success.NotRegistered -> {
+
+            }
+        }
+    }
+
+    private fun handleRegisteredState(state: LoginState.Success.Registered) = with(binding) {
+
+    }
+
+    private fun handleLoginState(state: LoginState.Login) {
+        val credential = GoogleAuthProvider.getCredential(state.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    viewModel.setUserInfo(user)
+                } else {
+                    firebaseAuth.signOut()
+                    viewModel.setUserInfo(null)
+                }
+            }
     }
 
 }
