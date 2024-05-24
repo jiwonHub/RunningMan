bfdc416781672074e57901f3f639115335281ffa
jiwon Choi <jiwon31273@naver.com>
2024-05-24 11:08:03 +0900

    [ADD] appModule.kt add

--- app/src/main/java/com/cjwjsw/runningman/domain/di/appModule.kt
+++ app/src/main/java/com/cjwjsw/runningman/domain/di/appModule.kt
@@ -1,9 +1,15 @@
 package com.cjwjsw.runningman.domain.di
 
+import com.cjwjsw.runningman.data.preference.AppPreferenceManager
+import com.cjwjsw.runningman.domain.factory.LoginViewModelFactory
+import com.cjwjsw.runningman.domain.factory.LoginViewModelFactoryImpl
 import com.cjwjsw.runningman.domain.usecase.GoogleLoginUseCase
 import com.cjwjsw.runningman.domain.usecase.KakaoLoginUseCase
+import com.cjwjsw.runningman.presentation.screen.login.LoginViewModel
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
+import dagger.assisted.AssistedFactory
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -23,4 +29,13 @@ object AppModule {
     fun provideGoogleLoginUseCase() : GoogleLoginUseCase {
         return GoogleLoginUseCase()
     }
+
+    @Provides
+    @Singleton
+    fun provideLoginViewModelFactory(
+        appPreferenceManager: AppPreferenceManager
+    ): LoginViewModelFactory {
+        return LoginViewModelFactoryImpl(appPreferenceManager)
+    }
+
 }
