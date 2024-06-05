fcd4b7cb6df707c6b05d8cee695d5ba03a8b01e5
centerfoward <jsu5438@naver.com>
2024-06-05 18:01:24 +0900

    [ADD] init

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/feed/feedScreen.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/feed/feedScreen.kt
@@ -2,11 +2,16 @@ package com.cjwjsw.runningman.presentation.screen.feed
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.cjwjsw.runningman.databinding.ActivityFeedMainBinding
 
 class feedScreen: AppCompatActivity() {
-    //private lateinit var binding
+    private lateinit var binding : ActivityFeedMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityFeedMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/feed/viewAdapter.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/feed/viewAdapter.kt
@@ -1,23 +1,31 @@
 package com.cjwjsw.runningman.presentation.screen.feed
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.cjwjsw.runningman.databinding.ItemFeedRecyclerviewBinding
+import com.cjwjsw.runningman.domain.model.FeedImageModel
 
-class viewAdapter() : RecyclerView.Adapter<viewAdapter.Holder>() {
+
+//데이터 테이블을 목록 형태로 보여주기 위한 어댑터
+// viewHoler ? 화면에 표시될 데이터나 아이템들을 저장하는 역할
+class viewAdapter (val uri : ArrayList<FeedImageModel>) : RecyclerView.Adapter<viewAdapter.Holder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewAdapter.Holder {
-        TODO("Not yet implemented")
+        //viewHolder가 생성되는 함수
+        //리사이클러뷰 정보 Holder로 넘기기
+        val binding = ItemFeedRecyclerviewBinding.inflate(LayoutInflater.from(parent.context))
+        return Holder(binding)
     }
 
     override fun onBindViewHolder(holder: viewAdapter.Holder, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.feedImg
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return uri.size
     }
 
     inner class Holder(val binding: ItemFeedRecyclerviewBinding) : RecyclerView.ViewHolder(binding.root){
-
+        val uri = binding.feedImg
     }
 }
