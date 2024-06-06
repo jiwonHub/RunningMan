1a1ea20a24ee8bba6f19ae06710aae0884372c51
jiwon Choi <jiwon31273@naver.com>
2024-06-06 22:34:43 +0900

    [ADD] build.gradle.kts play-services-auth add

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,8 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-auth:7.2.0")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
+
+    implementation ("com.google.android.gms:play-services-auth:21.2.0")
 }
 
 kapt {
