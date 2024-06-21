ebd540494f6db5a745e7174bfaafbf79dbd79c77
centerfoward <jsu5438@naver.com>
2024-06-21 13:36:37 +0900

    [FEAT] LoadProfileImge Feat

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/profile/ProfileFragment.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/profile/ProfileFragment.kt
@@ -1,16 +1,21 @@
 package com.cjwjsw.runningman.presentation.screen.main.fragment.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.cjwjsw.runningman.R
-import com.cjwjsw.runningman.databinding.FragmentMainBinding
-import com.cjwjsw.runningman.databinding.FragmentMapBinding
+import com.cjwjsw.runningman.core.UserManager
 import com.cjwjsw.runningman.databinding.FragmentProfileBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class ProfileFragment : Fragment() {
+
+@AndroidEntryPoint
+class ProfileFragment @Inject constructor() : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
@@ -18,6 +23,25 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding.addFeedBtn.setOnClickListener {
+            val intent = Intent(super.getActivity(), AddFeedActivity::class.java)
+            startActivity(intent)
+        }
+        loadProfileImg()
+
+
         return binding.root
     }
+
+    private fun loadProfileImg(){
+        val profileUrl = UserManager.getInstance()?.profileUrl
+        if (profileUrl != null) {
+            Glide.with(this)
+                .load(profileUrl)
+                .placeholder(R.drawable.sun)
+                .error(R.drawable.calories)
+                .into(binding.profileImg)
+        }
+    }
+
 }
