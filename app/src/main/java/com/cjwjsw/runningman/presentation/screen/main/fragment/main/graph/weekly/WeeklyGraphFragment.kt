e1ce5160f6d4e2069224d8bbc49ed544e38a7cbe
jiwon <jiwon30931@gmail.com>
2024-06-24 15:54:10 +0900

    [FEAT] WeeklyGraphFragment.kt bubble, date add

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/main/graph/weekly/WeeklyGraphFragment.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/main/graph/weekly/WeeklyGraphFragment.kt
@@ -1,22 +1,35 @@
 package com.cjwjsw.runningman.presentation.screen.main.fragment.main.graph.weekly
 
+import android.graphics.PorterDuff
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.cjwjsw.runningman.R
 import com.cjwjsw.runningman.databinding.FragmentWeeklyStatisticsBinding
+import com.cjwjsw.runningman.presentation.component.LabelUtils
+import com.cjwjsw.runningman.presentation.component.StatisticsDotLine
 
 import com.cjwjsw.runningman.presentation.component.StatisticsProgressBar
+import com.cjwjsw.runningman.service.WeeklyGraphFragmentListener
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Locale
 
 
-class WeeklyGraphFragment : Fragment() {
+class WeeklyGraphFragment : Fragment(), StatisticsProgressBar.BubbleListener {
     private var _binding: FragmentWeeklyStatisticsBinding? = null
     private val binding get() = _binding!!
     private var maxSteps = 1000
@@ -24,11 +37,19 @@ class WeeklyGraphFragment : Fragment() {
     private lateinit var progressBars: List<StatisticsProgressBar>
     private lateinit var average: MutableList<Int>
 
+    private lateinit var averageLabel: ImageView
+    private lateinit var averageLabelTextView: TextView
+    private lateinit var averageLabelLine: View
+    private lateinit var label6k: ImageView
+    private lateinit var textView6k: TextView
+    private lateinit var dotLine6k: View
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentWeeklyStatisticsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -42,107 +63,98 @@ class WeeklyGraphFragment : Fragment() {
             binding.fridayProgressBar,
             binding.saturdayProgressBar,
             binding.sundayProgressBar
+        ).apply {
+            forEach { it.setOnTouchListener { v, event ->
+                if (event.action == MotionEvent.ACTION_DOWN) {
+                    onProgressBarTouched(it as StatisticsProgressBar)
+                    true
+                } else {
+                    false
+                }
+            }}
+        }
+
+        averageLabel = LabelUtils.createImageView(requireContext())
+        averageLabelTextView = LabelUtils.createTextView(requireContext(), "평균")
+        averageLabelLine = LabelUtils.createCustomView(requireContext())
+        label6k = LabelUtils.create6kLabel(requireContext())
+        textView6k = LabelUtils.create6kTextView(requireContext())
+        dotLine6k = LabelUtils.create6kDotLine(requireContext())
+
+        binding.weeklyStatisticsLayout.addView(averageLabel)
+        binding.weeklyStatisticsLayout.addView(averageLabelTextView)
+        binding.weeklyStatisticsLayout.addView(averageLabelLine)
+
+        binding.weeklyStatisticsLayout.addView(label6k)
+        binding.weeklyStatisticsLayout.addView(textView6k)
+        binding.weeklyStatisticsLayout.addView(dotLine6k)
+
+        updateProgressBar(20000, 0)
+        updateProgressBar(8000, 1)
+        updateProgressBar(6000, 2)
+        updateProgressBar(3000, 3)
+        updateProgressBar(6000, 4)
+        updateProgressBar(8000, 5)
+        updateProgressBar(11000, 6)
+
+        updateLabels()
+        LabelUtils.addStepLabels(binding.weeklyStatisticsLayout, maxSteps, average)
+
+        binding.root.setOnClickListener {
+            hideAllBubbles()
+        }
+
+        binding.timeLine.text = getCurrentWeekDateRange()
+    }
+
+    private fun updateLabels() {
+        // 레이블 위치 업데이트
+        LabelUtils.setAverageLabel(
+            binding.weeklyStatisticsLayout, averageLabel, averageLabelTextView, averageLabelLine, average, maxSteps
         )
-
-        val label = createImageView()
-        val textView = createTextView()
-        val labelLine = createCustomView()
-
-        binding.weeklyStatisticsLayout.addView(label)
-        binding.weeklyStatisticsLayout.addView(textView)
-        binding.weeklyStatisticsLayout.addView(labelLine)
-
-        updateProgressBar(1000, 0)
-        updateProgressBar(800, 1)
-        updateProgressBar(600, 2)
-        updateProgressBar(300, 3)
-
-        setConstraintsForNewViews(binding.weeklyStatisticsLayout, label, textView, labelLine)
-    }
-
-    private fun createImageView(): ImageView { // 라벨 생성
-        return ImageView(requireContext()).apply {
-            id = View.generateViewId()
-            setImageResource(R.drawable.label)
-            layoutParams = ConstraintLayout.LayoutParams(
-                33.dpToPx(), // Convert dp to px
-                33.dpToPx()  // Convert dp to px
-            )
-        }
-    }
-
-    private fun createTextView(): TextView { // 평균 텍스트 생성
-        return TextView(requireContext()).apply {
-            id = View.generateViewId()
-            text = "평균"
-            setTextSize(10f)
-            setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
-            setTypeface(typeface, android.graphics.Typeface.BOLD)
-            layoutParams = ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.WRAP_CONTENT,
-                ConstraintLayout.LayoutParams.WRAP_CONTENT
-            )
-        }
-    }
-
-    private fun createCustomView(): View { // 평균 라인 생성
-        return View(requireContext()).apply {
-            id = View.generateViewId()
-            setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
-            layoutParams = ConstraintLayout.LayoutParams(
-                0,
-                3.dpToPx() // Convert dp to px
-            )
-        }
-    }
-
-    private fun setConstraintsForNewViews(
-        constraintLayout: ConstraintLayout,
-        imageView: ImageView,
-        textView: TextView,
-        customView: View
-    ) {
-        val constraintSet = ConstraintSet().apply {
-            clone(constraintLayout)
-            val nonZeroAverages = average.filter { it > 0 }
-            val average = if (nonZeroAverages.isNotEmpty()) nonZeroAverages.average() else 0.0
-            val height = 290 * (average / maxSteps)
-            val averagePx = (height + 13).toInt().dpToPx()
-
-            // ImageView constraints
-            connect(imageView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START)
-            connect(imageView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, averagePx)
-
-            // TextView constraints
-            connect(textView.id, ConstraintSet.START, imageView.id, ConstraintSet.START)
-            connect(textView.id, ConstraintSet.END, imageView.id, ConstraintSet.END)
-            connect(textView.id, ConstraintSet.TOP, imageView.id, ConstraintSet.TOP)
-            connect(textView.id, ConstraintSet.BOTTOM, imageView.id, ConstraintSet.BOTTOM)
-
-            // Custom View constraints
-            connect(customView.id, ConstraintSet.START, textView.id, ConstraintSet.END)
-            connect(customView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END)
-            connect(customView.id, ConstraintSet.TOP, imageView.id, ConstraintSet.TOP)
-            connect(customView.id, ConstraintSet.BOTTOM, imageView.id, ConstraintSet.BOTTOM)
-        }
-
-        constraintSet.applyTo(constraintLayout)
-    }
-
-    private fun Int.dpToPx(): Int {
-        return (this * resources.displayMetrics.density).toInt()
+        LabelUtils.set6kLabel(
+            binding.weeklyStatisticsLayout, label6k, textView6k, dotLine6k, maxSteps
+        )
     }
 
     private fun updateProgressBar(stepCount: Int, index: Int) = with(binding) {
-        val progress = (stepCount * 100) / maxSteps
-        val cappedProgress = progress.coerceAtMost(100)
+        if (stepCount > maxSteps) {
+            maxSteps = stepCount
+            progressBars.forEach { it.setMaxSteps(maxSteps) }
+            updateLabels()
+        }
 
         average[index] = stepCount
-        progressBars[index].animateProgress(cappedProgress)
+        val progressBar = progressBars[index]
+        progressBar.setProgressColor(stepCount > 6000)
+        progressBar.animateProgress(stepCount)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onProgressBarTouched(touchedProgressBar: StatisticsProgressBar) {
+        hideAllBubbles()
+        touchedProgressBar.showCustomBubble()
+    }
+
+    private fun hideAllBubbles() {
+        progressBars.forEach { it.hideCustomBubble() }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getCurrentWeekDateRange(): String {
+        val today = LocalDate.now()
+        val monday = today.with(java.time.DayOfWeek.MONDAY)
+        val sunday = today.with(java.time.DayOfWeek.SUNDAY)
+
+        val formatter = DateTimeFormatter.ofPattern("M월 d일", Locale.getDefault())
+        val startDateString = monday.format(formatter)
+        val endDateString = sunday.format(formatter)
+
+        return "$startDateString - $endDateString"
+    }
+
 }
