7611d2a9827fd021abb37bc4b440227a632cf58e
jiwon <jiwon30931@gmail.com>
2024-07-08 19:27:05 +0900

    [ADD] settings.gradle.kts naver map add

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven {
+            url = java.net.URI("https://repository.map.naver.com/archive/maven")
+        }
     }
 }
 dependencyResolutionManagement {
@@ -18,6 +21,9 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven {
+            url = java.net.URI("https://repository.map.naver.com/archive/maven")
+        }
     }
 }
 
