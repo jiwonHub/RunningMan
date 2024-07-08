854914fb6837d43a1c203b32d6cae0cbb902ec5d
jiwon <jiwon30931@gmail.com>
2024-07-08 19:26:23 +0900

    [ADD] MapFragment.kt navermap mapView add

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/map/MapFragment.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/map/MapFragment.kt
@@ -1,22 +1,93 @@
 package com.cjwjsw.runningman.presentation.screen.main.fragment.map
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.cjwjsw.runningman.R
-import com.cjwjsw.runningman.databinding.FragmentMainBinding
 import com.cjwjsw.runningman.databinding.FragmentMapBinding
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+    private lateinit var mapView: MapView
+    private lateinit var locationSource: FusedLocationSource
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        binding.currentLocationButton.map = naverMap
+        naverMap.locationSource = locationSource
+        val uiSetting = naverMap.uiSettings // 현위치 버튼
+        uiSetting.isLocationButtonEnabled = false
+
+        naverMap.maxZoom = 18.0 // 최대 줌
+        naverMap.minZoom = 10.0 // 최소 줌
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (!locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
