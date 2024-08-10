7da3c9f6998e24ab4d07d0708699a18b3d81de4c
centerfoward <jsu5438@naver.com>
2024-08-11 01:06:54 +0900

    [UI] UI Bottom Sheet Dialog

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/social/CommentModalBottomSheet.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/social/CommentModalBottomSheet.kt
@@ -6,35 +6,78 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import android.widget.FrameLayout
+import androidx.fragment.app.viewModels
 import com.cjwjsw.runningman.R
+import com.cjwjsw.runningman.databinding.DialogCommentBottomEdittextBinding
+import com.cjwjsw.runningman.databinding.DialogCommentBottomSheetModalBinding
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class CommentModalBottomSheet : BottomSheetDialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return BottomSheetDialog(requireContext(), R.style.BottomSheetDialogTheme)
-    }
+    lateinit var binding: DialogCommentBottomSheetModalBinding
+    lateinit var editBinding: DialogCommentBottomEdittextBinding
+    private val viewModel: DetailFeedViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.comment_bottom_sheet_modal, container, false)
+        binding = DialogCommentBottomSheetModalBinding.inflate(inflater, container, false)
+        editBinding = DialogCommentBottomEdittextBinding.inflate(inflater, container, false)
+        return inflater.inflate(R.layout.dialog_comment_bottom_sheet_modal, container, false)
     }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
+
+        bottomSheetDialog.setOnShowListener {
+            val constraintLayout =
+                dialog?.findViewById<FrameLayout>(com.google.android.material.R.id.container)
+
+            val editTextBinding =
+                DialogCommentBottomEdittextBinding.inflate(LayoutInflater.from(context))
+
+            val layoutParams = FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                Gravity.BOTTOM
+            )
+
+
+            constraintLayout?.addView(editTextBinding.root, layoutParams)
+            editTextBinding.root.viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    editTextBinding.root.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                    val height = editTextBinding.root.measuredHeight
+                    binding.root.setPadding(0, 0, 0, height)
+                }
+            })
+        }
+
+        return bottomSheetDialog
+
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       //버튼 리스너 등록
+
+        //버튼 리스너 등록
     }
 
     override fun onStart() {
         super.onStart()
         val bottomSheetDialog = dialog as BottomSheetDialog
-        val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
+        val bottomSheet =
+            bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
         bottomSheet?.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
         val behavior = BottomSheetBehavior.from(bottomSheet!!)
         behavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
@@ -44,29 +87,18 @@ class CommentModalBottomSheet : BottomSheetDialogFragment() {
         val bottomSheetCallback = object : BottomSheetBehavior.BottomSheetCallback() {
 
             override fun onStateChanged(bottomSheet: View, newState: Int) {
-                    when(newState){
-                        BottomSheetBehavior.STATE_COLLAPSED ->{
-                            dismiss()
-                        }
+                when (newState) {
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        dismiss()
                     }
+                }
             }
-
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
             }
         }
         behavior.addBottomSheetCallback(bottomSheetCallback)
-
-
     }
 
-    private fun ViewGroup.addBottomFixedItemView(view: View) {
-        val layoutParams = FrameLayout.LayoutParams(
-            FrameLayout.LayoutParams.MATCH_PARENT,
-            FrameLayout.LayoutParams.WRAP_CONTENT,
-            Gravity.BOTTOM
-        )
-        addView(view, layoutParams)
-    }
     companion object {
         const val TAG = "ModalBottomSheet"
     }
