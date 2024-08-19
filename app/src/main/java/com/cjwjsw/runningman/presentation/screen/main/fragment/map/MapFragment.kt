e37295145c69221ca509daaa405ab96527893773
jiwon Choi <jiwon31273@naver.com>
2024-08-19 11:49:36 +0900

    [ADD] MapFragment 폴리라인 긋기 & 리셋 버튼 추가

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/map/MapFragment.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/map/MapFragment.kt
@@ -1,24 +1,35 @@
 package com.cjwjsw.runningman.presentation.screen.main.fragment.map
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.cjwjsw.runningman.R
+import androidx.fragment.app.viewModels
+import com.cjwjsw.runningman.core.WalkDataSingleton
 import com.cjwjsw.runningman.databinding.FragmentMapBinding
+import com.cjwjsw.runningman.service.LocationUpdateService
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.PolylineOverlay
 import com.naver.maps.map.util.FusedLocationSource
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MapFragment : Fragment(), OnMapReadyCallback {
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
     private lateinit var mapView: MapView
     private lateinit var locationSource: FusedLocationSource
+    private val viewModel: MapViewModel by viewModels()
+    private var polyline: PolylineOverlay? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,6 +38,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         return binding.root
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
@@ -36,6 +48,42 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mapView = binding.mapView
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
+
+        startLocationService()
+
+        // 터치 리스너 설정
+        mapView.setOnTouchListener { v, event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                    binding.scrollView.requestDisallowInterceptTouchEvent(true)
+                }
+                MotionEvent.ACTION_UP -> {
+                    binding.scrollView.requestDisallowInterceptTouchEvent(false)
+                }
+            }
+            false
+        }
+
+        viewModel.address.observe(viewLifecycleOwner) { address ->
+            binding.locationText.text = address
+        }
+
+
+        binding.resetButton.setOnClickListener {
+            viewModel.resetData()
+
+            polyline?.map = null
+            polyline = PolylineOverlay().apply {
+                coords = emptyList()
+                map = null // 지우기 위해 null로 설정
+            }
+        }
+
+        requireActivity().startService(LocationUpdateService.newIntent(requireContext()))
+    }
+
+    private fun startLocationService() {
+        requireActivity().startService(LocationUpdateService.newIntent(requireContext()))
     }
 
     override fun onMapReady(naverMap: NaverMap) {
@@ -49,6 +97,30 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
         naverMap.maxZoom = 18.0 // 최대 줌
         naverMap.minZoom = 10.0 // 최소 줌
+
+        viewModel.path.observe(viewLifecycleOwner) { path ->
+            polyline?.map = null // 기존 폴리라인 제거
+            if (path.size > 1) {
+                polyline = PolylineOverlay().apply {
+                    coords = path
+                    color = Color.RED
+                    map = naverMap // 새로운 폴리라인 추가
+                }
+            }
+        }
+
+        // 현재 거리 UI 업데이트
+        updateDistanceUI(WalkDataSingleton.distance)
+
+        // 위치 업데이트 콜백 설정
+        naverMap.addOnLocationChangeListener { location ->
+            val latLng = LatLng(location.latitude, location.longitude)
+            viewModel.addLocation(latLng, requireContext())
+        }
+    }
+
+    private fun updateDistanceUI(distance: Double) {
+        binding.distance.text = String.format("%.1f", distance / 1000)
     }
 
     override fun onStart() {
@@ -63,7 +135,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onPause() {
         super.onPause()
-        mapView.onPause()
+//        mapView.onPause()
     }
 
     override fun onDestroyView() {
@@ -73,12 +145,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onStop() {
         super.onStop()
-        mapView.onStop()
+//        mapView.onStop()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView.onLowMemory()
+//        mapView.onLowMemory()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
