7a2d564955f8a4893b3e57e508ae59842402cadf
jiwon Choi <jiwon31273@naver.com>
2024-08-19 11:33:16 +0900

    [ADD] appModule.kt 날씨 레포 & DB & 로케이션 매니저 추가

--- app/src/main/java/com/cjwjsw/runningman/domain/di/appModule.kt
+++ app/src/main/java/com/cjwjsw/runningman/domain/di/appModule.kt
@@ -1,12 +1,19 @@
 package com.cjwjsw.runningman.domain.di
 
 import android.content.Context
+import androidx.room.Room
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
+import com.cjwjsw.runningman.core.LocationTrackerManager
+import com.cjwjsw.runningman.data.data_source.db.AppDatabase
+import com.cjwjsw.runningman.data.data_source.db.WalkDao
 import com.cjwjsw.runningman.data.data_source.weather.WeatherService
 import com.cjwjsw.runningman.data.preference.AppPreferenceManager
+import com.cjwjsw.runningman.data.repository.WalkRepositoryImpl
 import com.cjwjsw.runningman.data.repository.WeatherRepositoryImpl
+import com.cjwjsw.runningman.domain.repository.WalkRepository
 import com.cjwjsw.runningman.domain.repository.WeatherRepository
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import dagger.Binds
 import dagger.Module
@@ -29,6 +36,12 @@ abstract class RepositoryModule {
     abstract fun bindWeatherRepository(
         weatherRepositoryImpl: WeatherRepositoryImpl
     ): WeatherRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindWalkRepository(
+        walkRepositoryImpl: WalkRepositoryImpl
+    ): WalkRepository
 }
 
 
@@ -61,12 +74,16 @@ object AppModule {
         return FirebaseStorage.getInstance()
     }
 
-
-
     @Provides
     @Singleton
     fun provideWeatherService(retrofit: Retrofit): WeatherService {
         return retrofit.create(WeatherService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
     }
 
     @Provides
@@ -78,4 +95,28 @@ object AppModule {
     fun provideAppPreferenceManager(@ApplicationContext context: Context): AppPreferenceManager {
         return AppPreferenceManager(context)
     }
+
+    @Provides
+    @Singleton
+    fun provideLocationTrackerManager(): LocationTrackerManager {
+        return LocationTrackerManager()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_database" // 데이터베이스 이름
+        ).build()
+    }
+
+    // DAO 등록
+    @Provides
+    @Singleton
+    fun provideWalkDao(appDatabase: AppDatabase): WalkDao {
+        return appDatabase.walkDao()
+    }
+
 }
