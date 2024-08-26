5694fbb8527c508b3c94405438b28cd16ee22ff2
jiwon Choi <jiwon31273@naver.com>
2024-08-26 16:31:26 +0900

    [fix] scheduleDailyDataSync 불필요 코드 수정

--- app/src/main/java/com/cjwjsw/runningman/core/scheduleDailyDataSync.kt
+++ app/src/main/java/com/cjwjsw/runningman/core/scheduleDailyDataSync.kt
@@ -17,28 +17,16 @@ fun scheduleDailyDataSync(context: Context) {
     // 자정까지의 시간을 계산
     val now = Calendar.getInstance()
     val target = Calendar.getInstance().apply {
-        set(Calendar.HOUR_OF_DAY, 19)
-        set(Calendar.MINUTE, 52)
+        set(Calendar.HOUR_OF_DAY, 15)
+        set(Calendar.MINUTE, 20)
         set(Calendar.SECOND, 0)
         if (before(now)) add(Calendar.DAY_OF_YEAR, 1)
     }
     val initialDelay = target.timeInMillis - now.timeInMillis
-    val currentDistance = WalkDataSingleton.distance.value ?: 0.0
-    val currentStepCount = WalkDataSingleton.stepCount.value ?: 0
-    val currentCalories = WalkDataSingleton.calorie.value ?: 0.0
-    val currentTime = WalkDataSingleton.time.value ?: 0L
-    val data: Data = workDataOf(
-        "distance" to currentDistance,
-        "stepCount" to currentStepCount,
-        "calories" to currentCalories,
-        "time" to currentTime
-    )
-
 
     // 주기적 워크 요청 설정 (매일 반복)
     val workRequest = PeriodicWorkRequestBuilder<DataSyncWorker>(1, TimeUnit.DAYS)
         .setInitialDelay(initialDelay, TimeUnit.MILLISECONDS) // 처음 시작을 자정으로 맞춤
-        .setInputData(data)
         .build()
 
     WorkManager.getInstance(context).enqueueUniquePeriodicWork(
