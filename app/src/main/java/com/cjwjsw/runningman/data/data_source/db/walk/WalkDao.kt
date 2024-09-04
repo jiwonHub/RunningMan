d6f7a7d1a5766c6051bdc186fa9769ffb4ac33dd
jiwon Choi <jiwon31273@naver.com>
2024-09-04 19:13:31 +0900

    [FIX] DailyWalkEntity 이름 변경

--- app/src/main/java/com/cjwjsw/runningman/data/data_source/db/walk/WalkDao.kt
+++ app/src/main/java/com/cjwjsw/runningman/data/data_source/db/walk/WalkDao.kt
@@ -1,4 +1,4 @@
-package com.cjwjsw.runningman.data.data_source.db
+package com.cjwjsw.runningman.data.data_source.db.walk
 
 import androidx.room.Dao
 import androidx.room.Insert
@@ -9,19 +9,19 @@ import androidx.room.Query
 interface WalkDao {
 
     @Query("SELECT * FROM daily_walk")
-    suspend fun getAllWalks(): List<DailyWalk>
+    suspend fun getAllWalks(): List<DailyWalkEntity>
 
     @Query("SELECT * FROM daily_walk WHERE date = :date LIMIT 1")
-    suspend fun getWalkByDate(date: String): DailyWalk?
+    suspend fun getWalkByDate(date: String): DailyWalkEntity?
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertWalk(walk: DailyWalk)
+    suspend fun insertWalk(walk: DailyWalkEntity)
 
     @Query("SELECT COUNT(*) FROM daily_walk")
     suspend fun getWalkCount(): Int
 
     @Query("SELECT * FROM daily_walk WHERE date BETWEEN :startDate AND :endDate")
-    suspend fun getWalksBetweenDates(startDate: String, endDate: String): List<DailyWalk>
+    suspend fun getWalksBetweenDates(startDate: String, endDate: String): List<DailyWalkEntity>
 
     @Query("DELETE FROM daily_walk")
     suspend fun deleteAllWalks()
--- app/src/main/java/com/cjwjsw/runningman/domain/repository/WalkRepository.kt
+++ app/src/main/java/com/cjwjsw/runningman/domain/repository/WalkRepository.kt
@@ -1,16 +1,16 @@
 package com.cjwjsw.runningman.domain.repository
 
-import com.cjwjsw.runningman.data.data_source.db.DailyWalk
+import com.cjwjsw.runningman.data.data_source.db.walk.DailyWalkEntity
 
 interface WalkRepository {
 
-    suspend fun getWalkByDate(date: String): DailyWalk
+    suspend fun getWalkByDate(date: String): DailyWalkEntity
 
-    suspend fun insertWalk(walk: DailyWalk)
+    suspend fun insertWalk(walk: DailyWalkEntity)
 
-    suspend fun getAllWalks(): List<DailyWalk>
+    suspend fun getAllWalks(): List<DailyWalkEntity>
 
-    suspend fun getWalksBetweenDates(startDate: String, endDate: String): List<DailyWalk>
+    suspend fun getWalksBetweenDates(startDate: String, endDate: String): List<DailyWalkEntity>
 
     suspend fun getWalkCount(): Int
 
