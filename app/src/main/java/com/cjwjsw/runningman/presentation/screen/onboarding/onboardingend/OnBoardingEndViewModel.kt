075d21ef1b9ca69bb502945f416ed8a0dcc523eb
jiwon Choi <jiwon31273@naver.com>
2024-09-04 19:14:31 +0900

    [FEAT] OnBoardingEndScreen 사용자 정보 Room에도 저장

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/onboarding/onboardingend/OnBoardingEndViewModel.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/onboarding/onboardingend/OnBoardingEndViewModel.kt
@@ -3,13 +3,19 @@ package com.cjwjsw.runningman.presentation.screen.onboarding.onboardingend
 import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.cjwjsw.runningman.core.UserLoginFirst
+import com.cjwjsw.runningman.data.data_source.db.userInfo.UserInformationEntity
+import com.cjwjsw.runningman.domain.repository.UserInfoRepository
 import com.google.firebase.firestore.FirebaseFirestore
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class OnBoardingEndViewModel @Inject constructor() : ViewModel() {
+class OnBoardingEndViewModel @Inject constructor(
+    private val userInfoRepository: UserInfoRepository
+) : ViewModel() {
 
     private val db = FirebaseFirestore.getInstance()
 
@@ -33,6 +39,19 @@ class OnBoardingEndViewModel @Inject constructor() : ViewModel() {
                     Log.e("OnBoardingEndViewModel", "Error adding user data", e)
                     UserLoginFirst.setFirstLogin(context,false)
                 }
+        }
+    }
+
+    fun saveUserInfo(userId: String, age: Int, gender: String, height: Int, weight: Int) = viewModelScope.launch {
+        userId.let {
+            val userInfo = UserInformationEntity(
+                id = userId,
+                age = age,
+                gender = gender,
+                height = height,
+                weight = weight
+            )
+            userInfoRepository.insertUserInfo(userInfo)
         }
     }
 
