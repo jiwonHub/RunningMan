f3ca6ff51bf8f83ea2a78e28f33d0072a47f41d5
centerfoward <jsu5438@naver.com>
2024-09-04 19:17:19 +0900

    [FEAT] Feat Profile Feed Func

--- app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/profile/ProfileFeedDetailScreen.kt
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/profile/ProfileFeedDetailScreen.kt
@@ -1,17 +1,122 @@
 package com.cjwjsw.runningman.presentation.screen.main.fragment.profile
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.cjwjsw.runningman.R
+import com.cjwjsw.runningman.core.UserManager
 import com.cjwjsw.runningman.databinding.ActivitiyFeedDetailBinding
+import com.cjwjsw.runningman.presentation.screen.main.fragment.social.CommentModalBottomSheet
+import com.cjwjsw.runningman.presentation.screen.main.fragment.social.FeedDetailViewAdapter
+import com.cjwjsw.runningman.presentation.screen.main.fragment.social.ProfileFeedDetailViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProfileFeedDetailScreen : AppCompatActivity() {
     private lateinit var binding: ActivitiyFeedDetailBinding
-
+    private lateinit var viewPager : ViewPager2
+    private lateinit var adapter : FeedDetailViewAdapter
+    private var isLiked : Boolean = false
+    private val viewModel : ProfileFeedDetailViewModel by viewModels()
+    private var profileImg = ""
+    private var uid = ""
+    private var feedTitle = ""
+    private var userNumber = ""
+    private var IL = false
+    private var Lc = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitiyFeedDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        uid = intent.getStringExtra("UID").toString()
+        val image = intent.getStringArrayListExtra("URL")
+        profileImg = userData?.profileUrl!!
+        feedTitle = intent.getStringExtra("title").toString()
+        IL = intent.getBooleanExtra("isLiked",false) // 좋아요 눌렀는지 아닌지
+        Lc = intent.getIntExtra("likedCount",0) // 좋아요 개수
+
+
+        adapter = FeedDetailViewAdapter(image)
+        adapter = image?.let { FeedDetailViewAdapter(it) }!!
+        viewPager = binding.feedImgViewPager
+        viewPager.adapter = adapter
+        binding.indicator.setViewPager(binding.feedImgViewPager)
+        binding.title.text = feedTitle
+        binding.likedCountText.text = "${Lc}명이 좋아합니다"
+        userNumber = userData.userNumber
+
+        isLikedImg(IL)
+        loadProfileImg()
+        viewModel.getFeedUploadTime(uid)
+
+
+
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
+
+        viewModel.feed_time.observe(this){
+            binding.feedTime.text = it
+        }
+
+        viewModel.isLiked.observe(this){
+            isLikedImg(it)
+            isLiked = it
+        }
+        viewModel.likeCount.observe(this){ text ->
+            binding.likedCountText.text = "${text}명이 좋아합니다"
+        }
+
+        binding.likeBtn.setOnClickListener {
+            when(isLiked){
+                false -> {
+                    viewModel.getLikedCount(uid.toString())
+                }
+                true ->{
+                    viewModel.getLikedCount(uid.toString())
+                }
+            }
+        }
+
+        binding.commentBtn.setOnClickListener {
+            modalBottomSheet(uid.toString(), userName.toString(),userProfileImg.toString())
+        }
+    }
+
+
+    private fun loadProfileImg(){
+        Glide.with(this)
+            .load(profileImg)
+            .placeholder(R.drawable.sun)
+            .error(R.drawable.calories)
+            .into(binding.feedDetailProfileImage)
+    }
+
+
+
+    private fun modalBottomSheet(uid : String,userName : String,profileUrl : String) {
+        val modal = CommentModalBottomSheet(uid,userName,profileUrl,userNumber)
+
+        modal.show(supportFragmentManager, CommentModalBottomSheet.TAG)
+    }
+
+
+    private fun isLikedImg(liked : Boolean){
+        if(!liked){
+            binding.likeBtn.setImageResource(R.drawable.empty_like_btn)
+            isLiked = false
+        }else{
+            binding.likeBtn.setImageResource(R.drawable.like_icon)
+            isLiked = true
+        }
+    }
+    companion object{
+        val userData = UserManager.getInstance()
+        val userName = userData?.nickName
+        val userProfileImg = userData?.profileUrl
     }
 }
---
+++ app/src/main/java/com/cjwjsw/runningman/presentation/screen/main/fragment/profile/ProfileFeedDetailViewModel.kt
@@ -0,0 +1,173 @@
+package com.cjwjsw.runningman.presentation.screen.main.fragment.social
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.cjwjsw.runningman.core.UserManager
+import com.cjwjsw.runningman.domain.model.CommentModel
+import com.cjwjsw.runningman.domain.model.FeedModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.gson.Gson
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileFeedDetailViewModel @Inject constructor( private val firebaseFirestore: FirebaseFirestore) :
+    ViewModel(){
+    private val _likeCount = MutableLiveData<Int>()
+    val likeCount: LiveData<Int> get() = _likeCount
+
+    private val _isLiked = MutableLiveData<Boolean>()
+    val isLiked : LiveData<Boolean> get() = _isLiked
+
+    private val _commentArr = MutableLiveData<MutableList<CommentModel>?>()
+
+    val commentArr : LiveData<MutableList<CommentModel>?> get() = _commentArr
+
+    private val _feed_time = MutableLiveData<String>()
+    val feed_time : LiveData<String> get() = _feed_time
+
+
+    fun getLikedCount(uid : String){
+        val ref = firebaseFirestore.collection("posts").document(uid)
+
+        //데이터 받아오기
+        ref.get().addOnSuccessListener { document ->
+            if (document != null && document.exists()) {
+                Log.d("FeedViewModel", "받아온 데이터: ${document.data}")
+                val data = document.data?.toDataClass<FeedModel>()
+
+                val currentLikeCount = data?.likedCount ?: 0
+                val currentIsLiked = data?.isLiked ?: false
+
+                // 받아온 데이터 처리
+                val newCount = if (currentIsLiked) {
+                    currentLikeCount -1
+                } else {
+                    currentLikeCount +1
+                }
+
+                val newIsLiked = !currentIsLiked
+
+                Log.d("FeedViewModel",newIsLiked.toString())
+
+                //좋아요 카운트 업
+                ref.update("likedCount", newCount,"isLiked",newIsLiked)
+                    .addOnSuccessListener {
+                        Log.d("FeedViewModel", "LikedCount 업데이트 성공")
+                        _likeCount.value = newCount // Update the LiveData to reflect the new count
+                        _isLiked.value = newIsLiked
+                    }
+                    .addOnFailureListener { Log.d("FeedViewModel", "LikedCount 업데이트 실패") }
+            } else {
+                Log.d("FeedViewModel", "해당하는 UID의 피드가 없습니다")
+            }
+        }.addOnFailureListener { e ->
+            Log.w("FeedViewModel", "피드를 불러오는중 오류 발생", e)
+        }
+    }
+
+    fun fetchCommentData(uid : String){
+        val ref = FeedViewModel.fbRef.getReference(uid).child("comments")
+        Log.d("FeedViewModel", "uid = $uid")
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val commentList = mutableListOf<CommentModel>()
+                for (commentSnapshot in snapshot.children) {
+                    val comment = commentSnapshot.getValue(CommentModel::class.java)
+                    if (comment != null) {
+                        commentList.add(comment)
+                    }
+                }
+                _commentArr.value = commentList // _commentArr은 LiveData<List<Comment>> 타입
+                Log.d("FeedViewModel","댓글 불러오기 성공 : ${commentArr.value.toString()}")
+                Log.d("FeedViewModel","댓글 불러오기 성공 : ${snapshot.value.toString()}")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("FeedViewModel","댓글 불러오기 실패 : ${error.message}")
+            }
+
+        })
+    }
+
+    fun uploadComment(comment: String, feedUid: String, userName: String,profileImg: String,userNumber: String) {
+        val ref = FeedViewModel.fbRef.getReference(feedUid).child("comments")
+        val newComment = CommentModel(
+            comment = comment,
+            timestamp = System.currentTimeMillis() / 1000,
+            userName = userName,
+            profileUrl = profileImg,
+            userUid = userData?.idToken.toString(),
+            userNumber = userData?.userNumber.toString()
+        )
+
+        val newCommentKey = ref.push().key
+        if(newCommentKey != null) {
+            ref.child(newCommentKey).setValue(newComment).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("FeedViewModel", "Comment added successfully")
+                } else {
+                    Log.d("FeedViewModel", "Failed to add comment: ${task.exception}")
+                }
+            }
+        }
+    }
+
+    fun getFeedUploadTime(uid : String){
+        val ref = firebaseFirestore.collection("posts").document(uid)
+        ref.get().addOnSuccessListener {document ->
+            if (document != null && document.exists()) {
+                Log.d("FeedViewModel", "받아온 데이터: ${document.data}")
+                val data = document.data?.toDataClass<FeedModel>()
+
+                val time = data?.timestamp
+                Log.d("DetailFeedViewModel",time.toString())
+
+                // 피드 업로드 시간 가져오기
+                val uploadTimeInMillis = time?.seconds?.times(1000L) ?: 0L
+
+                // 현재 시간 가져오기
+                val currentTimeInMillis = System.currentTimeMillis()
+
+                // 업로드 시간과 현재 시간 뺴서 차이 구하기
+                val timeDifferenceInMillis = currentTimeInMillis - uploadTimeInMillis
+
+                // 밀리 세컨드 시간으로 변경
+                val hoursPassed = timeDifferenceInMillis / (1000 * 60 * 60)
+
+                // 밀리세컨드 분으로 변경
+                val minute = timeDifferenceInMillis / (1000 * 60)
+
+                if(hoursPassed in 1..12){
+                    //12시간이 안지났다면
+                    _feed_time.value = "${hoursPassed.toInt()}시간 전"
+                }else if(hoursPassed > 12){
+                    //12시간 초과
+                    _feed_time.value =  "${hoursPassed.toInt() / 12}일 전"
+                }else{
+                    // 1시간 미만이라면
+                    _feed_time.value = "${minute.toInt()}분 전"
+                }
+            }
+        }
+    }
+
+
+
+
+    private inline fun <reified T> Map<String, Any>.toDataClass(): T? {
+        val json = Gson().toJson(this)
+        return Gson().fromJson(json, T::class.java)
+    }
+
+    companion object{
+        val userData =  UserManager.getInstance()
+    }
+
+}
